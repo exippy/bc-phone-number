c46dac3f3ce2cc4954e18cbf82790d243e3af231
bennani driss <bennani.driss@pycraft.net>
2016-12-17 19:39:51 +0100

    updating build after fix

--- dist/js/bc-phone-number.js
+++ dist/js/bc-phone-number.js
@@ -1,6 +1,4 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.bcPhoneNumber = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-angular.module("bcPhoneNumberTemplates", []).run(["$templateCache", function($templateCache) {$templateCache.put("bc-phone-number/bc-phone-number.html","<section class=\"input-group\">\r\n  <label for=\"{{name}}\" ng-if=\"label\">{{label}}</label>\r\n  <div class=\"input-group-btn\" uib-dropdown uib-keyboard-nav>\r\n    <button type=\"button\" class=\"btn btn-default\" type=\"button\" uib-dropdown-toggle>\r\n      <span class=\"glyphicon iti-flag bc-phone-number-flag\" ng-class=\"selectedCountry.iso2Code\"></span><span class=\"caret\"></span>\r\n      <span class=\"sr-only\">Select country: {{selectedCountry.name}}</span>\r\n    </button>\r\n    <ul class=\"uib-dropdown-menu bc-phone-number-dropdown-menu dropdown-menu\" role=\"menu\">\r\n      <li ng-repeat=\"country in preferredCountries\" ng-click=\"selectCountry(country)\"\r\n          ng-class=\"{active: isCountrySelected(country)}\" role=\"menuitem\">\r\n        <a href=\"#\" ng-click=\"$event.preventDefault()\" class=\"bc-phone-number-country-anchor\">\r\n          <i class=\"glyphicon iti-flag bc-phone-number-country-icon\" ng-class=\"country.iso2Code\"></i>\r\n          <span ng-bind=\"country.name\"></span>\r\n        </a>\r\n      </li>\r\n      <li role=\"separator\" class=\"divider\" ng-show=\"preferredCountries && preferredCountries.length\"></li>\r\n      <li ng-repeat=\"country in allCountries\" ng-click=\"selectCountry(country)\"\r\n          ng-class=\"{active: isCountrySelected(country)}\" role=\"menuitem\">\r\n        <a href=\"#\" ng-click=\"$event.preventDefault()\" class=\"bc-phone-number-country-anchor\">\r\n          <i class=\"glyphicon iti-flag bc-phone-number-country-icon\" ng-class=\"country.iso2Code\"></i>\r\n          <span ng-bind=\"country.name\"></span>\r\n        </a>\r\n      </li>\r\n    </ul>\r\n  </div>\r\n  <input type=\"tel\" name=\"{{name}}\" id=\"{{name}}\" class=\"form-control\" ng-model=\"number\" ng-disabled=\"ngDisabled\" ng-change=\"changed()\"/>\r\n</section>\r\n");}]);
-},{}],2:[function(require,module,exports){
 (function (global){
 'use strict';
 
@@ -8,7 +6,7 @@ var bcCountries = (typeof window !== "undefined" ? window['bcCountries'] : typeo
 var angular = (typeof window !== "undefined" ? window['angular'] : typeof global !== "undefined" ? global['angular'] : null);
 
 global.angular = angular;
-require('../build/js/templates');
+
 
 angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
 .service('bcPhoneNumber', function() {
@@ -120,5 +118,5 @@ angular.module('bcPhoneNumber', ['bcPhoneNumberTemplates', 'ui.bootstrap'])
 module.exports = 'bcPhoneNumber';
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"../build/js/templates":1}]},{},[2])(2)
+},{}]},{},[1])(1)
 });
